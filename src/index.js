6750a74e38fb492a5a44c225e4e6963e23ac896d
Ricardo Gauto <ricardogauto11@gmail.com>
2019-02-24 17:28:24 -0300

    Importando firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD5nHdPYNtufMMUaSVHSbOoTeRPoM93Th0",
+    authDomain: "clom-251bc.firebaseapp.com",
+    databaseURL: "https://clom-251bc.firebaseio.com",
+    projectId: "clom-251bc",
+    storageBucket: "clom-251bc.appspot.com",
+    messagingSenderId: "815361454231"
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
